6a632b95fc182d70da9054dfa4d16f332941940f
LI-Jialu <55438527+LI-Jialu@users.noreply.github.com>
2020-10-27 23:04:10 +0800

    Update optimal_alignment.cpp

--- optimal_alignment.cpp
+++ optimal_alignment.cpp
@@ -1,13 +1,3 @@
-//CSCI3220 2020-21 First Term Assignment 1
-//I declare that the assignment here submitted is original except for source material explicitly acknowledged, and that the same or closely related material has not been previously submitted for another course.
-//I also acknowledge that I am aware of University policy and regulations on honesty in academic work,
-//and of the disciplinary guidelines and procedures applicable to breaches of such policy and regulations,
-//as contained in the following websites.
-//University Guideline on Academic Honesty:
-//http://www.cuhk.edu.hk/policy/academichonesty/
-//Student Name: LI Jialu
-//Student ID  : 1155107895
-
 #include <iostream>
 #include <vector>
 #include <string>
